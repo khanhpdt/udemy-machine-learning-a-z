7c0109d9f21c5e9c0548be93aa5788999147a31d
Khanh <khanhpdtvn@gmail.com>
2018-12-26 23:34:49 +0700

    Naive bayes + decision tree + random forest classifiers

---
+++ classification/naive_bayes/naive_bayes.py
@@ -0,0 +1,20 @@
+from data_preprocessing import data_preprocessing
+from sklearn.preprocessing import StandardScaler
+from sklearn.naive_bayes import GaussianNB
+from plots.classification_result_visualizer import visualize_two_feature_classification
+
+
+features, labels = data_preprocessing.import_dataset(
+        'datasets/Social_Network_Ads.csv', [2, 3], 4)
+
+feature_scaler = StandardScaler()
+features = feature_scaler.fit_transform(features)
+
+features_train, features_test, labels_train, labels_test = \
+    data_preprocessing.split_train_test(features, labels, test_size = 0.25)
+
+classifier = GaussianNB()
+classifier.fit(features, labels)
+
+visualize_two_feature_classification(features_train, labels_train, classifier, 
+                                     xlabel='Age', ylabel='Estimated salary')
